065132711649f52e93757e12210f3d88dc91ef09
David <David.Piruzashvili@gmail.com>
2021-05-07 20:45:02 -0400

    first step

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// console.log(1);
+let number = 5;
+
+
+// const arr = [];
+// arr[0] = prompt('What is your name?', '');
+// arr[1] = prompt('What is your surname?', '');
+// arr[2] = prompt('How old are you?','');
+// console.log(arr);
+
+const category= 'David';
+alert(`Hello, ${category}`);
