3f05126fcb9969f2473945ef75f574a80392dd03
David <David.Piruzashvili@gmail.com>
2021-05-08 15:37:10 -0400

    object creation

--- _JS_React/JS_React/js/script.js
+++ _JS_React/JS_React/js/script.js
@@ -1,2 +1,34 @@
 "use strict";
 
+
+const numberOfFilms = +prompt("How many films have you already seen?", "");
+console.log(numberOfFilms);
+
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+
+const lastFilm1 = prompt("One of the last film you saw?",''),
+
+ review1 = +prompt("Tell us your review?",''),
+ lastFilm2 = prompt("One of the last film you saw?",''),
+ review2 = +prompt("Tell us your review?",'');
+
+
+
+personalMovieDB.movies[lastFilm1] = review1;
+personalMovieDB.movies[lastFilm2] = review2;
+
+
+console.log(personalMovieDB);
+
+// var movies = {
+//     [answer1]: answer2
+// };
+
+// console.log(movies);
