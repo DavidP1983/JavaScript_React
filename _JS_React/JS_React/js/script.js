68d80fc18206b49730ae421d06bd17817155bb08
David <David.Piruzashvili@gmail.com>
2021-05-09 12:42:08 -0400

    loops and if practice

--- _JS_React/JS_React/js/script.js
+++ _JS_React/JS_React/js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
-
-const numberOfFilms = +prompt("How many films have you already seen?", "");
+// Exe 1
+/*const numberOfFilms = +prompt("How many films have you already seen?", "");
 console.log(numberOfFilms);
 
 var personalMovieDB = {
@@ -25,10 +25,66 @@ personalMovieDB.movies[lastFilm1] = review1;
 personalMovieDB.movies[lastFilm2] = review2;
 
 
+console.log(personalMovieDB);*/
+
+
+//*******************************************************************************************//
+
+//Exe 2 Loop
+
+var numberOfFilms = +prompt("How many films have you already seen?", "");
+console.log(numberOfFilms);
+
+
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+if (!personalMovieDB.count) {
+    alert("Произошла ошибка");
+} else if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+}
+// Solution #1
+/*var questions = [
+    "One of the last film you saw?",
+    "Tell us your review?",
+    "One of the last film you saw?",
+    "Tell us your review?"
+];
+for (var i = 0; i < questions.length; i++) {
+    var lastFilm1 = prompt(questions[i]);
+    if (!lastFilm1 || lastFilm1.length > 50) {
+        break;
+    }
+    i++;
+
+    var review1 = +prompt(questions[i]);
+    personalMovieDB.movies[lastFilm1] = review1;
+
+}*/
+
+// Solution #2
+
+for (let a = 0; a < 2; a++) {
+    const lastFilm1 = prompt("One of the last film you saw?", ''),
+          review1 = +prompt("Tell us your review?", '');
+
+          if(lastFilm1 !=null && review1 !=null && lastFilm1 != '' && review1 != '' && lastFilm1.length < 50 ){
+            personalMovieDB.movies[lastFilm1] = review1;
+            console.log("done");
+          }else{
+              console.log('Error');
+              a--;
+          }
+}
+
+
 console.log(personalMovieDB);
-
-// var movies = {
-//     [answer1]: answer2
-// };
-
-// console.log(movies);
