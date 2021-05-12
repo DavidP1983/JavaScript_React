53109e34665b782f387bf3e2df5873c30847801c
David <David.Piruzashvili@gmail.com>
2021-05-12 17:04:13 -0400

    Functions

--- _JS_React/JS_React/js/script.js
+++ _JS_React/JS_React/js/script.js
@@ -32,26 +32,27 @@ console.log(personalMovieDB);*/
 
 //Exe 2 Loop
 
-var numberOfFilms = +prompt("How many films have you already seen?", "");
-console.log(numberOfFilms);
+// var numberOfFilms = +prompt("How many films have you already seen?", "");
+// console.log(numberOfFilms);
 
 
-var personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-if (!personalMovieDB.count) {
-    alert("Произошла ошибка");
-} else if (personalMovieDB.count < 10) {
-    alert("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    alert("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    alert("Вы киноман");
-}
+// var personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false,
+// };
+// if (!personalMovieDB.count) {
+//     alert("Произошла ошибка");
+// } else if (personalMovieDB.count < 10) {
+//     alert("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     alert("Вы классический зритель");
+// } else if (personalMovieDB.count >= 30) {
+//     alert("Вы киноман");
+// }
+
 // Solution #1
 /*var questions = [
     "One of the last film you saw?",
@@ -73,18 +74,111 @@ for (var i = 0; i < questions.length; i++) {
 
 // Solution #2
 
-for (let a = 0; a < 2; a++) {
-    const lastFilm1 = prompt("One of the last film you saw?", ''),
-          review1 = +prompt("Tell us your review?", '');
+// for (let a = 0; a < 2; a++) {
+//     const lastFilm1 = prompt("One of the last film you saw?", ''),
+//           review1 = +prompt("Tell us your review?", '');
 
-          if(lastFilm1 !=null && review1 !=null && lastFilm1 != '' && review1 != '' && lastFilm1.length < 50 ){
-            personalMovieDB.movies[lastFilm1] = review1;
-            console.log("done");
-          }else{
-              console.log('Error');
-              a--;
-          }
+//           if(lastFilm1 !=null && review1 !=null && lastFilm1 != '' && review1 != '' && lastFilm1.length < 50 ){
+//             personalMovieDB.movies[lastFilm1] = review1;
+//             console.log("done");
+//           }else{
+//               console.log('Error');
+//               a--;
+//           }
+// }
+
+
+// console.log(personalMovieDB);
+
+
+//************************ Lesson Function********************//
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = prompt("How many films have you already seen?", "");
+    while (!numberOfFilms || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = prompt("How many films have you already seen?", "");
+
+    }
 }
 
+// start();
 
-console.log(personalMovieDB);
+var personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+
+function remember() {
+    for (let a = 0; a < 2; a++) {
+        const lastFilm1 = prompt("One of the last film you saw?", ''),
+            review1 = +prompt("Tell us your review?", '');
+
+        if (lastFilm1 != null && review1 != null && lastFilm1 != '' && review1 != '' && lastFilm1.length < 50) {
+            personalMovieDB.movies[lastFilm1] = review1;
+            console.log("done");
+        } else {
+            console.log('Error');
+            a--;
+        }
+    }
+}
+// remember();
+
+// console.log(personalMovieDB);
+
+function numberOfMovies() {
+    if (!personalMovieDB.count) {
+        alert("Произошла ошибка");
+    } else if (personalMovieDB.count < 10) {
+        alert("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        alert("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        alert("Вы киноман");
+    }
+}
+
+// numberOfMovies();
+
+//************************The same result of two functions************************************** */
+// function showDB(){
+//     if(personalMovieDB.privat == false){
+//         return personalMovieDB;
+//     }
+
+// }
+
+// console.log(showDB());
+
+function showDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+
+}
+
+showDB(personalMovieDB.privat);
+
+//*************************************************************** */
+
+function writeYourGenres() {
+    for (let i = 1; i < 4; i++) {
+        // var genres = prompt(`Your favorite gendre is numbered ${[i]}`);
+        // personalMovieDB.genres.push(genres);
+
+        personalMovieDB.genres[i - 1] = prompt(`Your favorite gendre is numbered ${i}`);
+        //в данном случае код работает быстрее
+
+        // personalMovieDB.genres[i -1] = genres;
+
+    }
+
+}
+
+writeYourGenres();
