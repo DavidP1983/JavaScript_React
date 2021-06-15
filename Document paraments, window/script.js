6f3fb162c9e97be46e36ab21cbbf47945dd64cdd
David <David.Piruzashvili@gmail.com>
2021-06-15 17:07:29 -0400

    Document's parameters, window

--- Document paraments, window/script.js
+++ Document paraments, window/script.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const box = document.querySelector('.box');
+const btn = document.querySelector('button');
+
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
+
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
+
+console.log(width, height);
+
+// const hideScoll = function(){
+// box.style.height = height + 'px';
+// };
+
+// btn.addEventListener('click', hideScoll);
+
+
+const hideScoll = function(){
+    console.log(box.scrollTop);
+    };
+    
+    btn.addEventListener('click', hideScoll);
+    
+
+// console.log(box.getBoundingClientRect().top);
+
+// const style = window.getComputedStyle(box);
+// console.log(style.display);
+
+
+// console.log(document.documentElement.scrollTop);
