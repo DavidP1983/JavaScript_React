cffff855131fd437f388001fe8235bdf7db8ad17
David <David.Piruzashvili@gmail.com>
2021-08-22 10:01:15 -0400

    Event Loop

---
+++ Event Loop/script.js
@@ -0,0 +1,29 @@
+// console.log(1);
+
+// setTimeout(() => {
+//     console.log('timeout_2000');
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log('timeout_3000');
+// }, 2000);
+
+// console.log(2);
+
+
+// let a = 0;
+
+// function count(){
+//     for(let i = 0; i < 1e9; i++){
+//         a++;
+//     }
+//     alert('done');
+// }
+
+// count();
+
+setTimeout(() =>{
+console.log(1);
+}, 0);
+
+console.log(2);
