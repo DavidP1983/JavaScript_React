ac67b13d911e71f19a24287f37225d92f34d0e27
David <David.Piruzashvili@gmail.com>
2021-08-25 09:02:05 -0400

    Eslint Config files

---
+++ React/script.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-tabs */
+const a = 2;
+console.log(a);
+
+const ab = {
+  name: 'Alex',
+  age: 25,
+  sayHello: () => {
+    console.log('hello');
+  },
+};
+
+ab.sayHello();
+
+const c = 'hello';
+
+console.log(c);
