1cd17fbd14f480f8c625f349168602ed0db9defa
David <David.Piruzashvili@gmail.com>
2021-10-26 12:04:12 -0400

    Class component, state

--- React_props/component-props/src/App.js
+++ React_props/component-props/src/App.js
@@ -1,22 +1,54 @@
-
+import { Component } from 'react';
 import './App.css';
 
 
-function WhoAmI({name,nameFun, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name.firstName}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-      <h2>My learning language is {nameFun()}</h2>   
-    </div>
-  )
+// function WhoAmI({name,nameFun, surname, link}) {
+//   return (
+//     <div>
+//       <h1>My name is {name.firstName}, surname - {surname}</h1>
+//       <a href={link}>My profile</a>
+//       <h2>My learning language is {nameFun()}</h2>   
+//     </div>
+//   )
+// }
+
+                                // Class Components
+
+class WhoAmI extends Component{
+  constructor(props) {
+    super(props);
+      this.state = {
+        years: 27,
+        text: "Change Years"
+      }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({
+      years: state.years + 1
+    }))
+  }
+
+  render(){
+    const{name, surname, link,nameFun} = this.props;
+    const{text} = this.state
+    return (
+      <div>
+        <button onClick={this.nextYear}>{text}</button>
+         <h1>My name is {name.firstName}, surname - {surname}, age - {this.state.years}</h1>
+         <a href={link}>My profile</a>
+         <h2>My learning language is {nameFun()}</h2>   
+     </div>
+    )
+  }
 }
+
 
 function App() {
   return (
     <div className="App">
         <WhoAmI name={{firstName: 'John'}} surname='Smith' link='facebook.com' nameFun={() => {return 'React'}}/>
-        <WhoAmI name='Alex' surname='Sheva' link='vk.com' nameFun={() => {return 'Node.js'}}/>
+        <WhoAmI name={{firstName: 'Alex'}} surname='Sheva' link='vk.com' nameFun={() => {return 'Node.js'}}/>
     </div>
   );
 }
