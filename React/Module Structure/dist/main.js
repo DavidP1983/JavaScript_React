488765ac6b32c9b9b2b1c09107fcd8fecd3740be
Tiger <silavsale@gmail.com>
2021-12-22 11:02:29 -0500

    Marvel project added, other projects removed

--- React/Module Structure/dist/main.js
+++
@@ -1,1 +0,0 @@
-(()=>{"use strict";new class{constructor(o,t,i){this.width=o,this.height=t,this.count=i}nextSlider(){console.log("Moving forward")}prevSlider(){console.log("Moving back")}whoAmI(){console.log(this.width,this.height,this.count)}}(300,400,4).whoAmI(),console.log("button")})();
