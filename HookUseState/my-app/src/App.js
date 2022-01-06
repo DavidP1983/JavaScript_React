ac88256bbea625c7d1a0d3d9ce99a9307a8fc8b9
David <David.Piruzashvili@gmail.com>
2022-01-06 09:57:41 -0500

    Hook useRef

--- HookUseState/my-app/src/App.js
+++ HookUseState/my-app/src/App.js
@@ -1,4 +1,4 @@
-import {Component,useState, useEffect, useCallback, useMemo} from 'react';
+import {Component,useState, useEffect, useCallback, useMemo, useRef} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
@@ -269,6 +269,45 @@ useEffect(() => {
   }
 
 
+
+                                                //-----Hook useRef-----//
+
+
+  const Form = () => {
+
+    const [text, setText] = useState('');
+
+    // const myRef = useRef(null);
+
+    const myRef = useRef(1);
+
+    // const focusFirstTI = () => {
+    //     myRef.current.focus();
+    // }
+
+    useEffect(() => {
+      // myRef.current++;
+      // console.log(myRef.current);
+      myRef.current = text;
+    })
+
+    return (
+        <Container>
+            <form className="w-50 border mt-5 p-3 m-auto">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
+                    <input /*ref={myRef}*/ onChange={(e) => setText(e.target.value)} type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    </div>
+                    <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
+                    <textarea  /*onClick={focusFirstTI} onClick={myRef.current++}*/  value={myRef.current} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div> 
+            </form>
+        </Container>
+    )
+}
+
+
 function App() {
   const [slider, setSlider] = useState(true);
 
@@ -276,6 +315,7 @@ function App() {
           <>
           <button onClick={() => setSlider(false)}>Click</button>
           {slider ? <Slider/> : null}
+          <Form/>
           </>
   );
 }
