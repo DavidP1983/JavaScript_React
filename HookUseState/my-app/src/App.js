1fc47d109823a9f4598c70edb69433cb3325080f
David <David.Piruzashvili@gmail.com>
2022-02-20 06:48:24 -0500

    HOC

--- HookUseState/my-app/src/App.js
+++ HookUseState/my-app/src/App.js
@@ -532,14 +532,14 @@ const InputComponent = () => {
 function reducer(state, action) {
   switch (action.type) {
     case 'toggle':
-      return {autoplay: !state.autoplay}
+      return { autoplay: !state.autoplay }
     case 'slow':
-      return {autoplay: 300}
-    case 'fast': 
-      return {autoplay: 700}
-    case 'customValue': 
-      return {autoplay: action.payLoad}
-    default: 
+      return { autoplay: 300 }
+    case 'fast':
+      return { autoplay: 700 }
+    case 'customValue':
+      return { autoplay: action.payLoad }
+    default:
       throw new Error();
   }
 }
@@ -547,7 +547,7 @@ function reducer(state, action) {
 const Slider1 = () => {
   const [slide, setSlide] = useState(0);
   // const [autoplay, setAutoplay] = useState(false);
-  const [autoplay, dispatch] = useReducer(reducer, {autoplay: false});
+  const [autoplay, dispatch] = useReducer(reducer, { autoplay: false });
 
 
   function changeSlide(i) {
@@ -568,16 +568,16 @@ const Slider1 = () => {
             onClick={() => changeSlide(1)}>+1</button>
           <button
             className="btn btn-primary me-2"
-            onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+            onClick={() => dispatch({ type: 'toggle' })}>toggle autoplay</button>
           <button
             className="btn btn-primary me-2"
-            onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+            onClick={() => dispatch({ type: 'slow' })}>slow autoplay</button>
           <button
             className="btn btn-primary me-2"
-            onClick={() => dispatch({type: 'fast'})}>faste autoplay</button>
-            <button
+            onClick={() => dispatch({ type: 'fast' })}>faste autoplay</button>
+          <button
             className="btn btn-primary me-2"
-            onClick={(e) => dispatch({type: 'customValue', payLoad: +(e.target.textContent)})}>1000</button>
+            onClick={(e) => dispatch({ type: 'customValue', payLoad: +(e.target.textContent) })}>1000</button>
         </div>
       </div>
     </Container>
@@ -588,6 +588,102 @@ const Slider1 = () => {
 
 
 
+// ----- Higher-Order Component, HOC ----- //
+
+const withSlider = (BaseComponent, getData) => {
+  return (props) => {
+    const [slide, setSlide] = useState(0);
+    const [autoplay, setAutoplay] = useState(false)
+  
+    useEffect(() => {
+      setSlide(getData());
+    }, [])
+  
+    function changeSlide(i) {
+      setSlide(slide => slide + i);
+    }
+    return <BaseComponent 
+    {...props}
+    slide={slide} 
+    autoplay={autoplay} 
+    changeSlide={changeSlide} 
+    setSlide={setSlide}
+    setAutoplay={setAutoplay}/>
+  }
+
+}
+
+const getDataFromFirstFetch = () => { return 10 };
+const getDataFromSecondFetch = () => { return 20 };
+
+const SliderFirst = (props) => {
+  
+
+  return (
+    <Container>
+      <div className="slider w-50 m-auto">
+        <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+        <div className="text-center mt-5">Active slide {props.slide}</div>
+        <div className="buttons mt-3">
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => props.changeSlide(-1)}>-1</button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => props.changeSlide(1)}>+1</button>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+const SliderSecond = (props) => {
+
+console.log(props.name);
+  return (
+    <Container>
+      <div className="slider w-50 m-auto">
+        <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+        <div className="text-center mt-5">Active slide {props.slide} <br />{props.autoplay ? 'auto' : null} </div>
+        <div className="buttons mt-3">
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => props.changeSlide(-1)}>-1</button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => props.changeSlide(1)}>+1</button>
+          <button
+            className="btn btn-primary me-2"
+            onClick={() => props.setAutoplay(autoplay => !props.autoplay)}>toggle autoplay</button>
+        </div>
+      </div>
+    </Container>
+  )
+}
+
+
+const SliderwithFirstFetch = withSlider(SliderFirst, getDataFromFirstFetch);
+const SliderwithSecondFetch = withSlider(SliderSecond, getDataFromSecondFetch);
+
+
+
+const withLogger = WrappedComponent => props => {
+  useEffect(() => {
+    console.log("first render!");
+  }, []);
+
+    return <WrappedComponent {...props} />
+}
+
+
+const Hello = () => {
+  return (
+    <h1>Hello</h1>
+  )
+
+}
+
+const HelloWithLogger = withLogger(Hello);
 
 function App() {
   const [slider, setSlider] = useState(true); // for useRef
@@ -642,8 +738,14 @@ function App() {
 
       </Provider>
 
-      <Slider1/>
-    </>
+      <Slider1 />
+
+
+     <SliderwithFirstFetch/>
+     <SliderwithSecondFetch name={"David"}/>
+     <HelloWithLogger/>
+
+   </>
   );
 }
 
